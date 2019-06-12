b851ecb63ba4d7d50bd312b0bdfdcf1ebc24bbcf
paqipig <1261805554@qq.com>
2019-06-13 00:06:58 +0800

    改变登陆的映射路径

--- src/main/java/com/example/demo/controller/BackPageController.java
+++ src/main/java/com/example/demo/controller/BackPageController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 后台页面映射
  */
 @Controller
 public class BackPageController {
+    @GetMapping("/login")
+    public String goLogin(){ return "back/login"; }
 
 }
