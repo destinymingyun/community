2b9646a00f80ae49d83af14eb4ac823b10bed756
destinymingyun <784172167@qq.com>
2019-06-12 23:19:57 +0800

    完成前台页面映射

--- src/main/java/com/example/demo/controller/FacePageController.java
+++ src/main/java/com/example/demo/controller/FacePageController.java
@@ -9,13 +9,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FacePageController {
 
+    /**
+     * 跳转到关于我们页面
+     * @return 返回关于我们页面映射
+     */
     @GetMapping("/about")
     public String goAbout() {
         return "face/about";
     }
 
+    /**
+     * 跳转到主页
+     * @return 返回主页映射
+     */
     @GetMapping({"/index", "/"})
     public String goIndex() {
         return "face/index";
+    }
+
+    /**
+     * 跳转到服务页面
+     * @return 服务页面映射
+     */
+    @GetMapping("/service")
+    public String goService() {
+        return "face/index";
+    }
+
+    /**
+     * 跳转到调节纠纷内容
+     * @return 返回调节纠纷内容
+     */
+    @GetMapping("/contact")
+    public String goContact() {
+        return "face/contact";
     }
 }
